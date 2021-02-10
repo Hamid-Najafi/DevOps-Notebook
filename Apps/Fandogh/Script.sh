74b73d9f38c07b97e9206e9bd1109573cc3a1e37
Hamid <Hamid.Najafi@email.com>
2021-02-10 18:11:23 +0330

    Update Script.sh

--- Apps/Fandogh/Script.sh
+++ Apps/Fandogh/Script.sh
@@ -1,9 +1,16 @@
 # -------==========-------
 # Fandogh CLI
 # -------==========-------
-pip install fandogh_cli --upgrade
+# Install Python
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.8
+sudo apt install python3-pip
+
+# Install Fandogh CLI
 pip3 install fandogh-cli --upgrade
 
+# Reload Shell and login to Fandogh
 fandogh login --username goldenstarc --password myzsyn-fucjys-0motjI
 
 fandogh namespace active --name goldenstarc
