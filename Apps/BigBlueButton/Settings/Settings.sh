0bac1e50e3edd7a8420d440b96045b58364ee6ea
Hamid <Hamid.Najafi@email.com>
2021-04-15 23:55:19 +0430

    inint

--- Apps/BigBlueButton/Settings/Settings.sh
+++ Apps/BigBlueButton/Settings/Settings.sh
@@ -2,13 +2,13 @@
 # Get new settings after update
 # -------==========-------
 export version=2.3.0-beta-3
-export fqdnHost=ib3.vir-gol.ir
+export fqdnHost=ib2.vir-gol.ir
 export user=ubuntu
 
-mkdir /Users/hamid/Development/Software/DevOps-Notebook/Apps/BigBlueButton/Settings/$version
-scp $user@$fqdnHost:/etc/nginx/sites-available/bigbluebutton  /Users/hamid/Development/Software/DevOps-Notebook/Apps/BigBlueButton/Settings/$version/bigbluebutton.nginx
-scp $user@$fqdnHost:/usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties /Users/hamid/Development/Software/DevOps-Notebook/Apps/BigBlueButton/Settings/$version/bigbluebutton.properties.orig
-scp $user@$fqdnHost:/usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml /Users/hamid/Development/Software/DevOps-Notebook/Apps/BigBlueButton/Settings/$version/settings.yml.orig
+mkdir /Users/hamid/Development/Software/DevOps-Notebook/Apps/BigBlueButton/Settings/$version/Orig
+scp $user@$fqdnHost:/etc/nginx/sites-available/bigbluebutton  /Users/hamid/Development/Software/DevOps-Notebook/Apps/BigBlueButton/Settings/$version//Orig/bigbluebutton.nginx
+scp $user@$fqdnHost:/usr/share/bbb-web/WEB-INF/classes/bigbluebutton.properties /Users/hamid/Development/Software/DevOps-Notebook/Apps/BigBlueButton/Settings/$version//Orig/bigbluebutton.properties
+scp $user@$fqdnHost:/usr/share/meteor/bundle/programs/server/assets/app/config/settings.yml /Users/hamid/Development/Software/DevOps-Notebook/Apps/BigBlueButton/Settings/$version//Orig/settings.yml
 
 # Check if annything changed in new version:
 https://www.diffchecker.com/diff
