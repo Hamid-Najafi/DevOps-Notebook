ceb8a5d92ef8ebdf89e406790fe37cf33603c05e
Hamid <Hamid.Najafi@email.com>
2023-12-11 15:06:54 +0330

    Init

---
+++ Apps/Nextcloud/Nextcloud.sh
@@ -0,0 +1,7 @@
+# -------==========-------
+# Docker Compose
+# -------==========-------
+mkdir -p ~/docker/postgres
+cp -R ~/DevOps-Notebook/Apps/Postgres/*  ~/docker/postgres
+cd  ~/docker/postgres
+docker-compose up -d
