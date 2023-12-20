b453102bc2946e8360c71a5a52c7849559320d86
Hamid Najafi <hamid.najafi@email.com>
2023-12-20 15:04:07 +0330

    Init

--- Apps/Traefik/Traefik.sh
+++ Apps/Traefik/Traefik.sh
@@ -1,20 +1,37 @@
 # -------==========-------
 # Traefik Docker Compose
 # -------==========-------
+# Make Directory
+sudo mkdir -p /mnt/data/traefik
+# Set Permissions
+sudo chmod 775 -R /mnt/data
+sudo chown -R $USER:docker /mnt/data
+
+# Create the docker volumes for the containers.
+docker volume create \
+      --driver local \
+      --opt type=none \
+      --opt device=/mnt/data/traefik \
+      --opt o=bind traefik-certificates
+
+# Clone Traefik Directory
 mkdir -p ~/docker
 cp -R ~/DevOps-Notebook/Apps/Traefik ~/docker/traefik
 cd ~/docker/traefik
-# nano docker-compose.yml
-# Set Host
-    #   - "traefik.http.routers.traefik.rule=Host(`hamid-najafi.ir`)"
-docker network create web
-docker compose up -d
+
+# Check and Edit .env file
+nano .env
+
+# Create Network and Run
 # Note: Check firewall & mapping rules for Port: 80 & 443
+docker network create traefik-network
+docker compose up -d
 # -------==========-------
 # Prometheus
 # -------==========-------
 export FQDN=hr.hamid-najafi.ir
 curl -u traefik:Traefikpass.24 http://$FQDN:9094/metrics/
+
 # -------==========-------
 # Tips
 # -------==========-------
