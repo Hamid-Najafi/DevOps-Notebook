478fe2d124d25d694efa5ec0e4a90a4bdc5e52e2
Hamid Najafi <hamid.najafi@email.com>
2024-01-08 12:09:37 +0330

    init

--- Apps/MicroTalk/MicroTalk BRO/MicroTalk BRO.sh
+++ Apps/MicroTalk/MicroTalk BRO/MicroTalk BRO.sh
@@ -4,7 +4,7 @@
 
 # Clone Traefik Directory
 mkdir -p ~/docker
-cp -R ~/DevOps-Notebook//MicroTalk/MicroTalk BRO ~/docker/mirotalkbro
+cp -R ~/DevOps-Notebook/Apps/MicroTalk/MicroTalk BRO ~/docker/mirotalkbro
 cd ~/docker/mirotalkbro
 
 # Check and Edit .env file
