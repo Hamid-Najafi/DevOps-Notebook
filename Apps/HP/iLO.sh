0f2ffbeb470f44d002e509715fe6106027072367
Hamid Najafi <hamid.najafi@email.com>
2024-01-17 14:35:50 +0330

    mail update

---
+++ Apps/HP/iLO.sh
@@ -0,0 +1,24 @@
+# -------==========-------
+# iLO
+# -------==========-------
+HP Integrated Lights-Out (iLO) Advanced License
+34RQQ-6D5R4-G8NW3-LBMG6-MLJXR
+325WC-J9QJ7-495NG-CP7WZ-7GJMM
+
+iLO 2 Advanced features have been activated License Key:
+
+License Key: 
+35DPH-SVSXJ-HGBJN-C7N5R-2SS4W
+32Q8Y-XZVGQ-4SGJB-4KY3R-M9ZBN
+
+MP Advanced
+iLO2
+35DPH-SVSXJ-HGBJN-C7N5R-2SS4W
+35SCR-RYLML-CBK7N-TD3B9-GGBW2
+iLO
+247RH-ZPJ8S-7B17D-FCE55-DDD17
+LO100i
+35DRP-7B3TX-78VVM-7KX4Y-XS74X – Advanced
+iLO MP (hp Integrity)
+32Q8Y-XZVGQ-4SGJB-4KY3R-M9ZBN – MP Advanced 
+32Q8Y-XZVGQ-4SGJB-4KY3R-M9ZBN – MP Advanced 
--- Apps/SigNoz/SigNoz.sh
+++ Apps/SigNoz/SigNoz.sh
@@ -2,3 +2,9 @@
 # Docker Compose
 # -------==========-------
 https://signoz.io/docs/install/docker/
+
+git clone -b main https://github.com/SigNoz/signoz.git && cd signoz/deploy/
+docker compose -f docker/clickhouse-setup/docker-compose.yaml up -d
+
+http://<IP-ADDRESS>:3301/
+http://172.25.10.8:3301/
