cfe3e1ebf4345d850871371d7ed7d634315b8cc5
Hamid <Hamid.Najafi@email.com>
2024-12-25 17:33:01 +0330

    iNIT

--- Apps/Orthanc/Orthanc.sh
+++ Apps/Orthanc/Orthanc.sh
@@ -6,6 +6,33 @@
 # AET Title of AE extention: "ORTHANC"
 # -------==========-------
 
+volumes:
+  orthanc-config:
+    external: true
+  orthanc-database:
+    external: true
+
+# Make Orthanc Directory
+sudo mkdir -p /mnt/data/orthanc/orthanc-config
+sudo mkdir -p /mnt/data/orthanc/orthanc-database
+
+# Set Permissions
+sudo chmod 750 -R /mnt/data/orthanc
+
+# Create the docker volumes for the containers.
+docker volume create \
+      --driver local \
+      --opt type=none \
+      --opt device=/mnt/data/nextcloud/nextcloud \
+      --opt o=bind orthanc-config
+
+docker volume create \
+      --driver local \
+      --opt type=none \
+      --opt device=/mnt/data/nextcloud/postgres \
+      --opt o=bind orthanc-database
+
+
 # Clone Orthanc Directory
 mkdir -p ~/docker
 cp -R ~/DevOps-Notebook/Apps/Orthanc ~/docker/orthanc
